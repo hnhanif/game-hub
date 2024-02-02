597c4ccfdda266e248d1b0e62af46005ad3cac79
Abu Hanif <hanifgp2500@gmail.com>
2024-02-02 10:58:11 +0600

    Fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,14 @@
+import useGenres from "../hooks/useGenres.ts";
+
+function GenreList() {
+  const { genres } = useGenres();
+  return (
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default GenreList;
